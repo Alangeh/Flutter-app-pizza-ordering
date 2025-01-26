eae1cf6a31071df9bab5de76c9e25534db601c45
Lionel Ngobesing <ngobesinglionel@gmail.com>
2025-01-26 15:09:52 +0100

    adding firebase completing login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:Pizza_App/app.dart';
+import 'package:Pizza_App/simple_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
